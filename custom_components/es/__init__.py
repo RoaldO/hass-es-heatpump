f1a24147a6a1694bbeb4a4375155d2aa2369d0d9
R. Osinga <roaldosinga@gmail.com>
2025-03-27 20:00:08 +0100

    some more logging because i still don't know where this is going

--- custom_components/es/__init__.py
+++ custom_components/es/__init__.py
@@ -11,10 +11,22 @@ from .const import DOMAIN
 _LOGGER = logging.getLogger("es")
 _LOGGER.debug("initialization started")
 
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+async def async_setup(hass: HomeAssistant, entry: ConfigEntry):
     """Set up the ES Heat Pump from a config entry."""
+    _LOGGER.debug("async_setup()")
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][entry.entry_id] = entry.data
+    _LOGGER.debug("async_setup():RETURN")
     return True
+
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Set up the ES Heat Pump from a config entry."""
+    _LOGGER.debug("async_setup_entry()")
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = entry.data
+    _LOGGER.debug("async_setup_entry():RETURN")
+    return True
+
 
 _LOGGER.debug("initialization done")
