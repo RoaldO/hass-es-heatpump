86d1a8d0b9ea55a6d05f133de0d6182c2072a1d7
R. Osinga <roaldosinga@gmail.com>
2025-04-02 20:48:38 +0200

    better mode setup

--- custom_components/es/const.py
+++ custom_components/es/const.py
@@ -1,3 +1,7 @@
+"""
+collection of all constants needed in the project so it provides an easier
+overview for the modules of what is available.
+"""
 import homeassistant.components.climate.const
 import homeassistant.const
 import voluptuous as vol
@@ -5,10 +9,19 @@ import voluptuous as vol
 DOMAIN = "es"
 PLATFORMS = ["climate"]
 
+HVAC_MODE_OFF = homeassistant.components.climate.const.HVACMode.OFF
 HVAC_MODE_HEAT = homeassistant.components.climate.const.HVACMode.HEAT
 HVAC_MODE_COOL = homeassistant.components.climate.const.HVACMode.COOL
-HVAC_MODE_OFF = homeassistant.components.climate.const.HVACMode.OFF
-SUPPORTED_MODES = [HVAC_MODE_HEAT, HVAC_MODE_COOL, HVAC_MODE_OFF]
+HVAC_MODE_SANITARY_HOT_WATER = "Sanitary hot water"
+HVAC_MODE_AUTO = "auto"
+
+SUPPORTED_MODES = [
+    HVAC_MODE_OFF,
+    HVAC_MODE_HEAT,
+    HVAC_MODE_COOL,
+    HVAC_MODE_SANITARY_HOT_WATER,
+    HVAC_MODE_AUTO,
+]
 
 TEMP_CELSIUS = homeassistant.const.UnitOfTemperature.CELSIUS
 ATTR_TEMPERATURE = homeassistant.const.ATTR_TEMPERATURE
